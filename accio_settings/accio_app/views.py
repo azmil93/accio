cacb5b4bfdcfe561f3f060a2103df8c7be8953f5
Hannah Hall <hannahkhall13@gmail.com>
2016-09-12 10:04:30 -0500

    recognize view and url

--- accio_settings/accio_app/views.py
+++ accio_settings/accio_app/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.core import serializers
+from accio_app.acr import acr
 
-# Create your views here.
+
+
+def index(request):
+    return render(request, 'accio/index.html')
+
+def recognize(request):
+    accio = acr.recognize(request.body)
+    return HttpResponse(accio)
