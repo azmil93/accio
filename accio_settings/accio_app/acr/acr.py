e742dac4a79d7774a082d4195161c31b1130e683
Hannah Hall <hannahkhall13@gmail.com>
2016-09-12 10:02:32 -0500

    acr package

---
+++ accio_settings/accio_app/acr/acr.py
@@ -0,0 +1,27 @@
+import os, sys
+from accio.acr.acrcloud.recognizer import ACRCloudRecognizer
+import pyaudio
+import wave
+import uuid
+from config_dict import config
+
+def recognize(data):
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    FILENAME = str(uuid.uuid4()) + ".wav"
+    p = pyaudio.PyAudio()
+    wf = wave.open(FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(data)
+    wf.close()
+
+    re = ACRCloudRecognizer(config)
+    song = re.recognize_by_file(FILENAME, 0)
+
+    #delete file
+    print(song)
+    return song
