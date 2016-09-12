30b9793ebd8e8b355b0ad9c36e8380f63fa488aa
Hannah Hall <hannahkhall13@gmail.com>
2016-09-12 15:29:30 -0500

    track model

--- accio_settings/accio_app/models.py
+++ accio_settings/accio_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Track(models.Model):
+    title = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    imageURL = models.CharField(max_length=200)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return title + artist
