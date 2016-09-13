ce9ce8c499518478cdc7d3be952f3507eef6bed7
Hannah Hall <hannahkhall13@gmail.com>
2016-09-13 15:45:25 -0500

    get user tracks and ng-repeat

--- accio_settings/accio_app/static/profile/profileCtrl.js
+++ accio_settings/accio_app/static/profile/profileCtrl.js
@@ -1,4 +1,24 @@
 angular.module('Accio_app')
-.controller('ProfileCtrl', function() {
+.controller('ProfileCtrl', function(DataFactory, $location) {
   const profile = this;
+  profile.title = 'welcome';
+  DataFactory.getUserTracks().then((res) => {
+    if (res.length > 1) {
+      profile.tracks = res
+    } else {
+      profile.noTracks = true;
+      profile.response = res.error;
+    }
+  })
+  profile.record = () => {
+    $location.path('/record')
+  }
+  profile.logout = () => {
+    DataFactory.logoutUser()
+    .then((res) => {
+      if (res.logout === true) {
+        $location.path('/')
+      }
+    })
+  }
 })
