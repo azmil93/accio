f7ef03fa12c0f211d16ca9ccbda03d9a139d5ab1
Hannah Hall <hannahkhall13@gmail.com>
2016-09-13 15:44:03 -0500

    register redirect

--- accio_settings/accio_app/static/login/loginCtrl.js
+++ accio_settings/accio_app/static/login/loginCtrl.js
@@ -2,13 +2,12 @@ angular.module('Accio_app')
 .controller('LoginCtrl', function(DataFactory, $location) {
   const login = this;
   login.hello = "Accio";
-  
+
   login.loginUser = () => {
     DataFactory.loginUser(login.user)
     .then((res) => {
       if (res[0]) {
-        var id = res[0].pk;
-        $location.path(`/profile/${id}`)
+        $location.path(`/profile`)
       }
       else {
         alert('Login Failed');
@@ -18,6 +17,13 @@ angular.module('Accio_app')
 
   login.register = () => {
     DataFactory.registerUser(login.createUser)
-    .then(res => console.log(res))
+    .then((res) => {
+      if (res[0]) {
+        $location.path(`/profile`)
+      }
+      else {
+        alert('Login Failed');
+      }
+    })
   };
 });
