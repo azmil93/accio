1f32d11d0077d55caf66752d71a0e97a3973bc8b
Hannah Hall <hannahkhall13@gmail.com>
2016-09-12 22:12:59 -0500

    model for track

--- accio_settings/accio_app/models.py
+++ accio_settings/accio_app/models.py
@@ -6,9 +6,9 @@ class Track(models.Model):
     title = models.CharField(max_length=100)
     album = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
-    imageURL = models.CharField(max_length=200)
+    imageURL = models.CharField(max_length=200, default='')
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return title + artist
+        return self.title + ' by ' + self.artist
