ee6bead5b3cc867baba41d0a32fd329aace3f1a5
Hannah Hall <hannahkhall13@gmail.com>
2016-09-15 10:32:10 -0500

    results showing on screen

--- accio_settings/accio_app/static/record/recordCtrl.js
+++ accio_settings/accio_app/static/record/recordCtrl.js
@@ -1,15 +1,28 @@
 angular.module('Accio_app')
 .controller('RecordCtrl', function($interval, $http, DataFactory) {
   const record = this;
+
   record.int = $interval(function () {
     if(record.recorded) {
-      console.log(record.recorded);
       var file = new FormData();
       file.append('audio/wav', record.recorded, 'output.wav');
-      DataFactory.recognize(file).then(res => console.log(res));
-      record.stopInterval()
+      DataFactory.recognize(file)
+        .then((res) => {
+          try {
+            if(res.status.code === 1001) {
+              record.noResults = true
+            }
+          }
+          catch(err) {
+            record.results = res[0].fields;
+            record.showInfo = true;
+          }
+        });
+      record.showResults = true;
+      record.stopInterval();
     }
-  }, 1000)
+  }, 1000);
+
   record.stopInterval = function () {
     $interval.cancel(record.int);
   };
